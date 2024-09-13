de5164703ea9d1e61531e575f7f980be035d25ce
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2024-09-13 09:53:10 -0400

    smart contracts

---
+++ contracts/VaultImplementationV1.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.27;
+
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract VaultImplementation is Initializable, ERC1967Upgrade, OwnableUpgradeable {
+
+    bytes32 public  name;
+    address public keeper;
+
+    struct TraderProps {
+        bool inverseCopyTrade;
+    }
+
+    mapping (address => TraderProps) trader;
+
+
+    function initialize(bytes32 _name) public initializer {
+        name = _name;
+        keeper = keeper;
+        __Ownable_init();
+    }
+
+}
---
+++ contracts/VaultProxy.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.27;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract VaultProxy is UUPSUpgradeable {
+    constructor(address initialImpl) {
+        _upgradeTo(initialImpl);
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override {}
+}
